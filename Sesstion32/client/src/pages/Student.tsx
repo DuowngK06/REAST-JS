6b8252cbcb366c723194adb0747eb02157e275aa
dh5147666 <dh5147666@gmail.com>
2025-09-25 21:40:58 +0700

    update code

---
+++ Sesstion32/client/src/pages/Student.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Student() {
+    type User = {
+        id: number
+        name: string 
+        
+    }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Student
