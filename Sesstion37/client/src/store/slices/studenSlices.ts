d4bf4ad3763ea7e69c6a6526a0b473755dedca70
dh5147666 <dh5147666@gmail.com>
2025-09-26 08:22:59 +0700

    uodate code

---
+++ Sesstion37/client/src/store/slices/studenSlices.ts
@@ -0,0 +1,8 @@
+import { createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
+       const StudentSlice = createSlice({
+         name: 'student',
+         initialState: {
+
+         }
+         
---
+++ Sesstion37/client/src/store/sore.ts
@@ -0,0 +1,8 @@
+import {configureStore} from "@reduxjs/toolkit"
+import studen_slient from "./slices/studenSlice"
+const storeRedux = confixgureStone({
+    reducer{
+        students:student_slice
+    }
+})
+export default storeRedex;
